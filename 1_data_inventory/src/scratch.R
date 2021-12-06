5bac4c22bb2c10dcbb400d79a74e15032e5b9e70
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-12-06 09:01:02 -0500

    refine Georgia data inventory

--- 1_data_inventory/src/scratch.R
+++ 1_data_inventory/src/scratch.R
@@ -9,7 +9,7 @@ ggplot(x, aes(x = Year, y = value, fill = name)) +
   facet_wrap(~ntiles, scales = 'free_y')
 
 
-
+library(readr)
 library(ggridges)
 library(hrbrthemes)
 data <- ga_all_yrs %>%
@@ -68,3 +68,103 @@ g <- data %>%
   ylab('Real Losses (Million Gallons)')
   
 ggplotly(g, tooltip = 'text')
+
+
+
+
+
+
+ga_all_yrs <- read_csv('C:/Users/ggorski/OneDrive - DOI/USGS_ML/WaterUse/github/municipal-water-loss-ml/1_data_inventory/out/georgia_all_years.csv',show_col_types = FALSE)
+
+ca_all_yrs <- read_csv('C:/Users/ggorski/OneDrive - DOI/USGS_ML/WaterUse/github/municipal-water-loss-ml/Infrastructure_data/Infrastructure_data/California/CA_water_audit_data.csv')
+
+ca_pwsid <- data.frame(state = 'CA', pwsid = ca_all_yrs$pwsid %>% unique())
+ga_pwsid <- data.frame(state = 'GA', pwsid = ga_all_yrs$WSID %>% unique())
+
+
+ca_ga_pwsid <- rbind(ca_pwsid, ga_pwsid)
+
+#write_csv(ca_ga_pwsid, '1_data_inventory/out/ca_ga_pwsid.csv')
+
+library(plotly)
+library(hrbrthemes)
+library(viridis)
+library(gghalves)
+library(ggdist)
+library(readr)
+
+p <- ga_all_yrs %>%
+  filter(!is.na(Population)) %>%
+  mutate(ntiles = ntile(Population,5)) %>%
+  mutate(ntiles_f = ordered(ntiles, levels = c(1,2,3,4,5), 
+                          labels = c('Population < 4,853', 'Population 4,855 - 7,062', 'Population 7,073 - 13,050',
+                                     'Population 13,260 - 28,025','Population > 28,1832'))) %>%
+  select(Year, Util, Population, Supplied, Non.Revenue.Water, Real.Losses, Apparent.Losses, ntiles_f) %>%
+  mutate(Unbilled.Authorized.Consumption = Non.Revenue.Water-(Real.Losses+Apparent.Losses)) %>%
+  mutate(Real.Losses.pct = (Real.Losses/Supplied), Apparent.Losses.pct = (Apparent.Losses/Supplied), 
+         Unbilled.Authorized.Consumption.pct = (Unbilled.Authorized.Consumption/Supplied))%>%
+  group_by(ntiles_f, Year) %>%
+  summarise(Real.Losses = median(Real.Losses.pct), Apparent.Losses = median(Apparent.Losses.pct), 
+            Unbilled.Authorized.Consumption = median(Unbilled.Authorized.Consumption.pct))%>%
+  #filter(Util == 'Adel') %>%
+  pivot_longer(cols = Real.Losses:Unbilled.Authorized.Consumption) %>%
+  ggplot(aes(x = Year, y = value, fill = name, text = name))+
+  geom_area()+
+  scale_fill_viridis(discrete = TRUE, name = 'Non-Revenue Category')+
+  #geom_line(data = supp_data, aes(x = Year, y = Supplied))+
+  theme_bw()+
+  ggtitle('Non-Revenue Water by Municipality Population')+
+  ylab('Water Losses as a fraction of water supplied')+
+  facet_wrap(.~ntiles_f, ncol = 1, scales = "fixed")
+p
+
+#ggplotly(p)
+my_pal <- viridis(5)
+
+#Real water lost
+p <- ga_all_yrs %>%
+  filter(!is.na(Population)&!is.na(Real.Losses)) %>%
+  mutate(ntiles = ntile(Population,5)) %>%
+  mutate(ntiles_f = ordered(ntiles, levels = c(1,2,3,4,5), 
+                            labels = c('Population < 4,853', 'Population 4,855 - 7,062', 'Population 7,073 - 13,050',
+                                       'Population 13,260 - 28,025','Population > 28,1832'))) %>%
+  ggplot(aes(y = ntiles_f, x = Real.Losses, fill = ntiles_f, color = ntiles_f))+
+  stat_density_ridges(alpha = 0.8, quantiles = 2, quantile_lines = TRUE)+
+  theme_ipsum()+
+  xlim(0,1250)+
+  xlab('Real water losses \n (Millions of Gallons)')+
+  ylab('Population served')+
+  labs(title = 'Water lost 2012-2019')+
+  theme(legend.position = 'none', plot.title = element_text(size = 16, face = "bold"),
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.title.x = element_text(size = 14))+
+  scale_color_manual(values = rep('black',5), guide = "none") +
+  scale_fill_manual(values = my_pal, guide = "none")
+
+p
+
+#Apparent water lost
+p <- ga_all_yrs %>%
+  filter(!is.na(Population)&!is.na(Apparent.Losses)) %>%
+  mutate(ntiles = ntile(Population,5)) %>%
+  mutate(ntiles_f = ordered(ntiles, levels = c(1,2,3,4,5), 
+                            labels = c('Population < 4,853', 'Population 4,855 - 7,062', 'Population 7,073 - 13,050',
+                                       'Population 13,260 - 28,025','Population > 28,1832'))) %>%
+  ggplot(aes(y = ntiles_f, x = Apparent.Losses, fill = ntiles_f, color = ntiles_f))+
+  stat_density_ridges(alpha = 0.8, quantiles = 2, quantile_lines = TRUE)+
+  theme_ipsum()+
+  xlim(0,250)+
+  xlab('Apparent water losses \n (Millions of Gallons)')+
+  ylab('Population served')+
+  labs(title = 'Water lost 2012-2019')+
+  theme(legend.position = 'none', plot.title = element_text(size = 16, face = "bold"),
+        axis.text.x = element_text(size = 14),
+        axis.text.y = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.title.x = element_text(size = 14))+
+  scale_color_manual(values = rep('black',5), guide = "none") +
+  scale_fill_manual(values = my_pal, guide = "none")
+
+p
